dc084b037cd24408d09d5da435dc1349d67029e4
cysdavid <cysdavid@ucla.edu>
2025-04-22 15:33:44 -0700

    makes folders for cy and clement

---
+++ cy/plot_spinup.py
@@ -0,0 +1,165 @@
+"""
+Plot planes from joint analysis files.
+
+Usage:
+    plot_spinup.py <sim_name> [--stride=<strd>]
+
+Options:
+    --stride=<strd>  Interval of snapshots to plot [default: 1]
+"""
+
+import h5py
+import json
+from docopt import docopt
+import glob
+import pathlib
+import numpy as np
+from scipy.interpolate import RegularGridInterpolator
+import matplotlib.pyplot as plt
+
+from dedalus.tools.general import natural_sort
+from dedalus.tools.parallel import Sync
+
+from mpi4py import MPI
+
+rank = MPI.COMM_WORLD.rank
+size = MPI.COMM_WORLD.size
+
+args = docopt(__doc__)
+sim_name = args['<sim_name>']
+strd = int(args['--stride'])
+data_path = pathlib.Path('data').absolute()
+params_path = pathlib.Path('params').absolute()
+save_data_path = data_path.joinpath(sim_name)
+static_fields_path = save_data_path.joinpath('static_fields')
+save_params_path = params_path.joinpath(sim_name+".json")
+data_files = natural_sort(glob.glob(str(save_data_path.joinpath('*.h5'))))
+static_fields_files = natural_sort(glob.glob(str(static_fields_path.joinpath('*.h5'))))
+
+# Load parameters
+with open(save_params_path) as f: 
+    params= json.load(f)
+
+# Strides for quiver plot
+quivspacing = 0.02
+s_quivstrd = int(quivspacing/(params['Ls']/params['ns']))
+z_quivstrd = int(quivspacing/((params['Lz']+2*params['w'])/params['nz']))
+
+with Sync() as sync:
+    if sync.comm.rank == 0:
+        print("Getting time series...")
+
+# Get time series and maxima
+
+DelOmega_arr = []
+t_arr = []
+rel_uphi_maxes = []
+uphi_maxes = []
+us_maxes = []
+uz_maxes = []
+
+for filename in data_files:
+    with h5py.File(filename, mode='r') as f:
+        if filename == data_files[0]:
+            s_data = f['tasks']['uphi'].dims[2][0][:]
+            z_data = f['tasks']['uphi'].dims[1][0][:]
+
+            ss_data = np.outer(np.ones(len(z_data)),s_data)
+            zz_data = np.outer(z_data,np.ones(len(s_data)))
+
+            ss_reg = np.outer(np.ones(params['nz']),np.linspace(s_data[0],s_data[-1],params['ns']))
+            zz_reg = np.outer(np.linspace(z_data[0],z_data[-1],params['nz']),np.ones(params['ns']))
+
+        uphi_rel_maxes = []
+        for it in range(0,f['tasks']['DelOmega'].shape[0],strd):
+            DelOmega_field = f['tasks']['DelOmega']
+            DelOmega_i = DelOmega_field[it][0]
+            t_i = DelOmega_field.dims[0]['sim_time'][it]
+            DelOmega_arr.append(DelOmega_i)
+            t_arr.append(t_i)
+            
+            uphi_rel_maxes.append(np.max(np.abs(f['tasks']['uphi'][it]-DelOmega_i*ss_data)))
+
+        file_uphi_rel_max = np.max(uphi_rel_maxes)
+        file_uphi_max = np.max(np.abs(f['tasks']['uphi'][::strd]))
+        file_us_max = np.max(np.abs(f['tasks']['us'][::strd]))
+        file_uz_max= np.max(np.abs(f['tasks']['uz'][::strd]))
+        rel_uphi_maxes.append(file_uphi_rel_max)
+        uphi_maxes.append(file_uphi_max)
+        us_maxes.append(file_us_max)
+        uz_maxes.append(file_uz_max)
+
+DelOmega_arr = np.array(DelOmega_arr)
+t_arr = np.array(t_arr)
+rel_uphi_max = np.max(rel_uphi_maxes)
+uphi_max = np.max(uphi_maxes)
+us_max = np.max(us_maxes)
+uz_max = np.max(uz_maxes)
+
+# Create output directory if needed
+frame_path = pathlib.Path('frames').absolute()
+output_path = frame_path.joinpath(sim_name)
+
+with Sync() as sync:
+    if sync.comm.rank == 0:
+        print("Done.\n")
+        if not frame_path.exists():
+            frame_path.mkdir()
+        if not output_path.exists():
+            output_path.mkdir()
+
+        print("Plotting...")
+
+# Import data and plot
+for filename in data_files:
+    with h5py.File(filename, mode='r') as f:
+        for it in range(strd*rank,f['tasks']['uphi'].shape[0],strd*size):
+            
+            write_num = f['tasks']['uphi'].dims[0]['write_number'][it]
+            DelOmega_data = f['tasks']['DelOmega'][it][0][0]
+            uphi_data = f['tasks']['uphi'][it]
+            us_data = f['tasks']['us'][it]
+            uz_data = f['tasks']['uz'][it]
+        
+            t_data = f['tasks']['E_phi'].dims[0]['sim_time'][it]
+
+            us_interp_fxn = RegularGridInterpolator((z_data,s_data),us_data, bounds_error=False)
+            us_reg = us_interp_fxn((zz_reg,ss_reg))
+
+            uz_interp_fxn = RegularGridInterpolator((z_data,s_data),uz_data, bounds_error=False)
+            uz_reg = uz_interp_fxn((zz_reg,ss_reg))
+
+            fig, axs = plt.subplot_mosaic([['time','field'],['prof','field']],figsize=(7.5,5))
+            
+            axs['time'].plot(t_arr,1+DelOmega_arr)
+            axs['time'].scatter(t_data,1+DelOmega_data)
+            axs['time'].set_ylabel(r"$\Omega(t)/\Omega_0$")
+            axs['time'].set_xlabel(r"$t \sqrt{\nu \Omega_0}/H$")
+            
+            pm = axs['field'].pcolormesh(ss_data,zz_data,(uphi_data - DelOmega_data*ss_data),cmap='RdBu_r',vmin=(-rel_uphi_max,rel_uphi_max))
+            plt.colorbar(pm,ax=axs['field'],extend='both',label=r"$u_\phi/(\Omega H)$")
+            axs['field'].quiver(ss_reg[::z_quivstrd,::s_quivstrd],zz_reg[::z_quivstrd,::s_quivstrd],us_reg[::z_quivstrd,::s_quivstrd],uz_reg[::z_quivstrd,::s_quivstrd],scale=np.sqrt(us_max**2+uz_max**2)*10)
+            axs['field'].axhline(params['Lz']/2,color='k')
+            axs['field'].axhline(-params['Lz']/2,color='k')
+            axs['field'].set_aspect(1)
+            axs['field'].set_xlabel(r'$s/H$')
+            axs['field'].set_ylabel(r'$z/H$')
+
+            axs['prof'].plot(s_data,(uphi_data - DelOmega_data*ss_data)[np.argmin(np.abs(z_data)),:])
+            axs['prof'].set_ylim(-rel_uphi_max,rel_uphi_max)
+            axs['prof'].grid()
+            axs['prof'].set_xlabel(r'$s/H$')
+            axs['prof'].set_ylabel(r"$u_\phi/(\Omega H)$")
+
+            plt.suptitle(f"$t \\sqrt{{\\nu \\Omega_0}}/H = {t_data:.2f}$")
+
+            plt.subplots_adjust(hspace=0.4)
+
+            savepath = output_path.joinpath('write_%06i.jpg' %(write_num))
+
+            if rank==0:
+                print(f"Saving image {int(write_num/strd)}/{len(t_arr)}\r", end="")
+
+            plt.savefig(str(savepath), dpi=300)
+            plt.close()
+        
---
+++ cy/spinup.py
@@ -0,0 +1,185 @@
+import numpy as np
+from mpi4py import MPI
+import time
+import json
+
+from dedalus import public as d3
+
+import logging
+logger = logging.getLogger(__name__)
+
+from dedalus.tools.parallel import Sync
+import pathlib
+
+rank = MPI.COMM_WORLD.rank
+size = MPI.COMM_WORLD.size
+
+######### PARAMETERS ###############################################################
+
+# Simulation name
+sim_name = 'sim0'
+
+# Numerical Parameters
+ns, nz = (128,256)
+dealias = 3/2
+dtype = np.float64
+timestepper = d3.RK443 #d3.RK222
+
+# Physical parameters
+Ek = 1e-1 # Ekman number, Ek = nu/(Omega*H**2)
+PeakOmega = 0.1 # Maximum (absolute) change in rotation rate
+Lz = 1 # height of cylinder
+Ls = 0.5 # radius of cylinder
+w = 0.1 # thickness of top and bottom "lids"
+eta = 3e-4  # Volume penalty damping timescale (enforces no-slip at top and bottom), 
+            # set eta << 1 or eta < Ek to be safe
+
+# Boundary forcing function, i.e., how the tank rotation rate should vary with time, t
+# Examples:
+
+## Spin-down/up:
+# delay = 1e-2
+# DelOmega_func = lambda t : PeakOmega * (0.5*(1 + np.tanh((2*(-2*delay + t))/delay)))
+
+## Spin-down then spin-up:
+DelOmega_func = lambda t : PeakOmega * -1/np.cosh((t - 0.05)/0.005)
+
+## Write your own function:
+# DelOmega_func = lambda t : <your function of t>
+
+# Cadences and stop time
+timestep = 1e-4
+output_cadence = 10
+stop_sim_time = 0.1
+snapshot_dt = stop_sim_time/1000
+
+######### SIMULATION CODE ##########################################################
+
+# Create bases and domain
+coords = d3.CartesianCoordinates('z', 's') # Fourier direction comes first, for parallelization
+dist = d3.Distributor(coords, dtype=np.float64)
+sbasis = d3.Chebyshev(coords['s'], ns, bounds=(0, Ls), dealias=3/2)
+zbasis = d3.RealFourier(coords['z'], nz, bounds=(-Lz/2-w, Lz/2+w), dealias=3/2)
+zgrid, sgrid = dist.local_grids(zbasis, sbasis)
+
+# Fields
+s = dist.Field(name='s', bases=sbasis)
+s['g'] = sgrid
+z = dist.Field(name='z', bases=zbasis)
+z['g'] = zgrid
+p = dist.Field(name='p', bases=(zbasis,sbasis))
+us = dist.Field(name='us', bases=(zbasis,sbasis))
+uphi = dist.Field(name='uphi', bases=(zbasis,sbasis))
+uz = dist.Field(name='uz', bases=(zbasis,sbasis))
+t = dist.Field()
+
+# Boundary forcing
+DelOmega = DelOmega_func(t)
+
+# Substitutions
+ds = lambda A: d3.Differentiate(A, coords['s'])
+dz = lambda A: d3.Differentiate(A, coords['z'])
+tank_integ = lambda A: 2*np.pi*d3.Integrate(s*A, ('z','s'))
+Vol = np.pi*Ls**2*(Lz+2*w)
+tank_avg = lambda A: 1/Vol * tank_integ(A)
+integ_z = lambda A: d3.Integrate(A, ('z'))
+
+# Tau terms p, us2, uphi2, uz2
+tau_p = dist.Field(name='tau_p', bases=zbasis)
+tau_us2 = dist.Field(name='tau_us2', bases=zbasis)
+tau_uphi2 = dist.Field(name='tau_uphi2', bases=zbasis)
+tau_uz2 = dist.Field(name='tau_uz2', bases=zbasis)
+lift_basis = sbasis.derivative_basis(1)
+lift = lambda A: d3.Lift(A, lift_basis, -1)
+ds_us = ds(us)
+ds_uphi = ds(uphi)
+ds_uz = ds(uz)
+
+# Volume penalty terms
+mask = lambda x: 0.5*(1+np.tanh(-2*x))
+eps = (eta*Ek)**(1/2)
+delta = 2.64822828*eps # sharpness of mask for obstacle
+Kceil = dist.Field(name='Kceil', bases=(zbasis))
+Kfloor = dist.Field(name='Kfloor', bases=(zbasis))
+Kceil['g'] = mask(-(zgrid - (Lz/2))/delta)
+Kfloor['g'] = mask((zgrid - (-Lz/2))/delta)
+
+# Problem
+problem = d3.IVP([p, us, uphi, uz, tau_p, tau_us2, tau_uphi2, tau_uz2], time=t, namespace=locals())
+## Equations
+problem.add_equation("-2*s**2*uphi + Ek*us + s**2*ds(p) - Ek*s*ds_us - Ek*s**2*ds(ds_us) + np.sqrt(Ek)*s**2*dt(us) - Ek*s**2*dz(dz(us)) + s**2*lift(tau_us2) = s*uphi**2 - s**2*us*ds_us - s**2*uz*dz(us) - s**2*((Kceil+Kfloor)*us)/eta")
+problem.add_equation("Ek*uphi + 2*s**2*us - Ek*s*ds_uphi - Ek*s**2*ds(ds_uphi) + np.sqrt(Ek)*s**2*dt(uphi) - Ek*s**2*dz(dz(uphi)) + s**2*lift(tau_uphi2) = -(s*uphi*us) - s**2*us*ds_uphi - s**2*uz*dz(uphi) - s**2*((Kceil+Kfloor)*(uphi-DelOmega*s))/eta")
+problem.add_equation("-(Ek*ds_uz) - Ek*s*ds(ds_uz) + np.sqrt(Ek)*s*dt(uz) + s*dz(p) - Ek*s*dz(dz(uz)) + s*lift(tau_uz2) = -(s*us*ds_uz) - s*uz*dz(uz) - s*((Kceil+Kfloor)*uz)/eta")
+problem.add_equation("us + s*ds_us + s*dz(uz) + tau_p = 0")
+## BCs
+problem.add_equation("us(s=Ls) = 0")
+problem.add_equation("uphi(s=Ls) = DelOmega*Ls")
+problem.add_equation("uz(s=Ls) = 0")
+problem.add_equation("p(s=0) = 0", condition="nz==0") # Sets pressure gauge
+problem.add_equation("ds(us)(s=Ls) = 0", condition="nz!=0") # Consequence of continuity eqn at the wall
+
+# Build solver
+solver = problem.build_solver(timestepper)
+logger.info('Solver built')
+
+# Integration parameters
+solver.stop_sim_time = stop_sim_time
+
+# Create data and params directories if needed
+data_path = pathlib.Path('data').absolute()
+params_path = pathlib.Path('params').absolute()
+with Sync() as sync:
+    if sync.comm.rank == 0:
+        if not data_path.exists():
+            data_path.mkdir()
+        if not params_path.exists():
+            params_path.mkdir()
+save_data_path = data_path.joinpath(sim_name)
+static_fields_path = save_data_path.joinpath('static_fields')
+save_params_path = params_path.joinpath(sim_name+".json")
+
+# Save parameters
+# Ek, D, Lz, Ls, w, eta, switch_s, switch_phi, switch_z
+if rank == 0:
+    params_dict = {'ns':ns,'nz':nz,'timestep':timestep,
+                   'Ek':Ek,'PeakOmega':PeakOmega,'Lz':Lz,'Ls':Ls,
+                   'w':w,'eta':eta}
+    params_json = json.dumps(params_dict, indent = 4)
+    with open(save_params_path, "w") as outfile: 
+        outfile.write(params_json)
+
+# Analysis
+snapshots = solver.evaluator.add_file_handler(str(save_data_path), sim_dt=snapshot_dt, max_writes=1000)
+snapshots.add_task(us)
+snapshots.add_task(uphi)
+snapshots.add_task(uz)
+snapshots.add_task(p)
+snapshots.add_task(DelOmega,name='DelOmega')
+snapshots.add_task(dz(us)-ds(uz),name='vort_phi')
+snapshots.add_task(tank_avg(uphi),name='avg_uphi')
+snapshots.add_task(0.5*tank_avg(uphi**2),name='E_phi')
+snapshots.add_task(0.5*tank_avg(us**2 + uz**2),name='E_sz')
+
+static_fields = solver.evaluator.add_file_handler(str(static_fields_path), sim_dt = stop_sim_time)
+static_fields.add_task(Kceil)
+static_fields.add_task(Kfloor)
+
+# Flow properties
+flow = d3.GlobalFlowProperty(solver, cadence=output_cadence)
+flow.add_property(np.abs(uphi), name='abs_uphi')
+flow.add_property(np.abs(uz), name='abs_uz')
+flow.add_property(np.abs(p), name='abs_p')
+
+# Main loop
+try:
+    logger.info('Starting loop')
+    while solver.proceed:
+        solver.step(timestep)
+        if (solver.iteration-1) % output_cadence == 0:
+            logger.info('Iteration: %i, Time: %e, dt: %e' %(solver.iteration, solver.sim_time, timestep))
+            logger.info('Max |uphi| = {}, Max |uz| = {}, Max |p| = {}'.format(flow.max('abs_uphi'), flow.max('abs_uz'), flow.max('abs_p')))
+except:
+    logger.error('Exception raised, triggering end of main loop.')
+    raise
+finally:
+    solver.log_stats()
