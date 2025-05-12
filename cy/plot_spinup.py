c0ed04d694d88a86119ae6c48e250b0d60e8bb7c
cysdavid <cysdavid@ucla.edu>
2025-05-11 18:34:09 -0700

    axisymm. ivp: imposes free surface condition at z=0
    parameters scale to 36 cm wide tank w/ 2 cm fluid depth (from free surface to base) rotating initially at 11 RPM. sim took 20 cpu-hours

--- cy/plot_spinup.py
+++ cy/plot_spinup.py
@@ -40,8 +40,19 @@ static_fields_files = natural_sort(glob.glob(str(static_fields_path.joinpath('*.
 with open(save_params_path) as f: 
     params= json.load(f)
 
+# Check if sim has free surface
+if "free_surface" in params.keys():
+    free_surface = params["free_surface"]
+else:
+    free_surface = False
+    
 # Strides for quiver plot
-quivspacing = 0.02
+if free_surface:
+    quivspacing = 1/75 * max(params['Ls'],(params['Lz']/2 + params['w']))
+    aspect_ratio = params['Ls']/(params['Lz']/2)
+else:
+    quivspacing = 1/75 * max(params['Ls'],(params['Lz'] + 2*params['w']))
+    aspect_ratio = params['Ls']/(params['Lz'])
 s_quivstrd = int(quivspacing/(params['Ls']/params['ns']))
 z_quivstrd = int(quivspacing/((params['Lz']+2*params['w'])/params['nz']))
 
@@ -51,12 +62,13 @@ with Sync() as sync:
 
 # Get time series and maxima
 
-vort_quant = 0.99
+vort_quant = 0.98 #0.99
 
 DelOmega_arr = []
 t_arr = []
 rel_uphi_maxes = []
 uphi_maxes = []
+mom_maxes = []
 vort_maxes = []
 us_maxes = []
 uz_maxes = []
@@ -75,6 +87,7 @@ for filename in data_files:
 
         uphi_rel_maxes = []
         file_vort_maxes = []
+        file_mom_maxes = []
         for it in range(0,f['tasks']['DelOmega'].shape[0],strd):
             DelOmega_field = f['tasks']['DelOmega']
             DelOmega_i = DelOmega_field[it][0]
@@ -84,17 +97,20 @@ for filename in data_files:
             
             uphi_rel_maxes.append(np.nanmax(np.abs(f['tasks']['uphi'][it]-DelOmega_i*ss_data)))
             file_vort_maxes.append(np.nanquantile(np.abs(f['tasks']['vort_phi'][it]),q=vort_quant))
+            file_mom_maxes.append(np.nanquantile(np.abs(ss_data*f['tasks']['uphi'][it] + ss_data**2),q=vort_quant))
 
         file_uphi_rel_max = np.nanmax(uphi_rel_maxes)
         file_uphi_max = np.nanmax(np.abs(f['tasks']['uphi'][::strd]))
         file_us_max = np.nanmax(np.abs(f['tasks']['us'][::strd]))
         file_uz_max= np.nanmax(np.abs(f['tasks']['uz'][::strd]))
         file_vort_max = np.nanquantile(file_vort_maxes,q=vort_quant)
+        file_mom_max = np.nanquantile(file_mom_maxes,q=vort_quant)
         rel_uphi_maxes.append(file_uphi_rel_max)
         uphi_maxes.append(file_uphi_max)
         us_maxes.append(file_us_max)
         uz_maxes.append(file_uz_max)
         vort_maxes.append(file_vort_max)
+        mom_maxes.append(file_mom_max)
 
 DelOmega_arr = np.array(DelOmega_arr)
 t_arr = np.array(t_arr)
@@ -103,6 +119,7 @@ uphi_max = np.max(uphi_maxes)
 us_max = np.max(us_maxes)
 uz_max = np.max(uz_maxes)
 vort_max = np.quantile(vort_maxes,q=vort_quant)
+mom_max = np.max(mom_maxes)
 
 # Create output directory if needed
 frame_path = pathlib.Path('frames').absolute()
@@ -138,44 +155,73 @@ for filename in data_files:
             uz_interp_fxn = RegularGridInterpolator((z_data,s_data),uz_data, bounds_error=False)
             uz_reg = uz_interp_fxn((zz_reg,ss_reg))
 
-            fig, axs = plt.subplot_mosaic([['time','field','vort'],['prof','field','vort']],figsize=(0.5*7.5 + 15*params['Ls']/params['Lz'],5))
-            
+            if params['Ls'] < params['Lz']:
+                scale_fig_size = params['Ls']/(params['Lz'])
+                fig_height = 1/scale_fig_size*3
+                fig_width = fig_height * 1/(aspect_ratio/(1+3*aspect_ratio) * 1/aspect_ratio)
+                hspace = 0.4
+                wspace = 0.4
+                fig, axs = plt.subplot_mosaic([['time','uphi','vort','mom'],['prof','uphi','vort','mom']],figsize=(fig_width,fig_height),width_ratios=[0.5,aspect_ratio,aspect_ratio,aspect_ratio])
+                quiv_scale_units = 'width'
+                plt.subplots_adjust(hspace=hspace,wspace=wspace)
+            else:
+                scale_fig_size = params['Ls']/(params['Lz'])
+                fig_width = scale_fig_size*6
+                fig_height = (fig_width * aspect_ratio/(1+aspect_ratio) * 3/aspect_ratio)
+                hspace = 0.4
+                fig, axs = plt.subplot_mosaic([['time','uphi'],['prof','vort'],['.','mom']],figsize=(fig_width,fig_height),width_ratios=[1,aspect_ratio])
+                quiv_scale_units = 'height'
+                plt.subplots_adjust(hspace=hspace)
+                axs['vort'].set_ylabel(r'$z/H$')
+                axs['mom'].set_ylabel(r'$z/H$')
+
             axs['time'].plot(t_arr,1+DelOmega_arr)
             axs['time'].scatter(t_data,1+DelOmega_data)
             axs['time'].set_ylabel(r"$\Omega(t)/\Omega_0$")
             axs['time'].set_xlabel(r"$t \sqrt{\nu \Omega_0}/H$")
             
-            pm = axs['field'].pcolormesh(ss_data,zz_data,(uphi_data - DelOmega_data*ss_data),cmap='RdBu_r',vmin=(-rel_uphi_max,rel_uphi_max),shading='gouraud')
-            plt.colorbar(pm,ax=axs['field'],extend='both',label=r"$u_\phi/(\Omega H)$")
-            axs['field'].quiver(ss_reg[::z_quivstrd,::s_quivstrd],zz_reg[::z_quivstrd,::s_quivstrd],us_reg[::z_quivstrd,::s_quivstrd],uz_reg[::z_quivstrd,::s_quivstrd],scale=np.sqrt(us_max**2+uz_max**2)*5)
-            axs['field'].axhline(params['Lz']/2,color='k')
-            axs['field'].axhline(-params['Lz']/2,color='k')
-            axs['field'].set_aspect(1)
-            axs['field'].set_xlabel(r'$s/H$')
-            axs['field'].set_ylabel(r'$z/H$')
+            pm = axs['uphi'].pcolormesh(ss_data,zz_data,(uphi_data - DelOmega_data*ss_data),cmap='RdBu_r',vmin=(-rel_uphi_max,rel_uphi_max),shading='gouraud')
+            plt.colorbar(pm,ax=axs['uphi'],extend='both',label=r"$u_\phi/(\Omega_0 H)$")
+            axs['uphi'].quiver(ss_reg[::z_quivstrd,::s_quivstrd],zz_reg[::z_quivstrd,::s_quivstrd],us_reg[::z_quivstrd,::s_quivstrd],uz_reg[::z_quivstrd,::s_quivstrd],scale=np.sqrt(us_max**2+uz_max**2)*5,scale_units=quiv_scale_units)
+            axs['uphi'].axhline(params['Lz']/2,color='k')
+            axs['uphi'].axhline(-params['Lz']/2,color='k')
+            if free_surface:
+                axs['uphi'].set_ylim(-params['Lz']/2-params['w'],0)
+            axs['uphi'].set_aspect(1)
+            axs['uphi'].set_xlabel(r'$s/H$')
+            axs['uphi'].set_ylabel(r'$z/H$')
 
             pm = axs['vort'].pcolormesh(ss_data,zz_data,vort_data,cmap='PuOr_r',vmin=(-vort_max,vort_max))
-            plt.colorbar(pm,ax=axs['vort'],extend='both',label=r"$\Omega^{-1} (\mathbf{\nabla} \times \mathbf{u}) \cdot \mathbf{e}_\phi$")
+            plt.colorbar(pm,ax=axs['vort'],extend='both',label=r"$\Omega_0^{-1} (\mathbf{\nabla} \times \mathbf{u}) \cdot \mathbf{e}_\phi$")
             axs['vort'].axhline(params['Lz']/2,color='k')
             axs['vort'].axhline(-params['Lz']/2,color='k')
+            if free_surface:
+                axs['vort'].set_ylim(-params['Lz']/2-params['w'],0)
             axs['vort'].set_aspect(1)
             axs['vort'].set_xlabel(r'$s/H$')
+
+            pm = axs['mom'].pcolormesh(ss_data,zz_data,(ss_data*uphi_data + ss_data**2),cmap='RdYlBu',vmin=(0,mom_max),shading='gouraud')
+            plt.colorbar(pm,ax=axs['mom'],extend='both',label=r"$(s^2 \Omega(t) + s u_\phi)/(\Omega_0 H^2)$")
+            axs['mom'].axhline(params['Lz']/2,color='k')
+            axs['mom'].axhline(-params['Lz']/2,color='k')
+            if free_surface:
+                axs['mom'].set_ylim(-params['Lz']/2-params['w'],0)
+            axs['mom'].set_aspect(1)
+            axs['mom'].set_xlabel(r'$s/H$')
 
             axs['prof'].plot(s_data,(uphi_data - DelOmega_data*ss_data)[np.argmin(np.abs(z_data)),:])
             axs['prof'].set_ylim(-rel_uphi_max,rel_uphi_max)
             axs['prof'].grid()
             axs['prof'].set_xlabel(r'$s/H$')
-            axs['prof'].set_ylabel(r"$u_\phi/(\Omega H)$")
+            axs['prof'].set_ylabel(r"$u_\phi/(\Omega_0 H)$")
 
             plt.suptitle(f"$t \\sqrt{{\\nu \\Omega_0}}/H = {t_data:.2f}$")
-
-            plt.subplots_adjust(hspace=0.4)
 
             savepath = output_path.joinpath('write_%06i.jpg' %(write_num))
 
             if rank==0:
                 print(f"Saving image {int(write_num/strd)}/{len(t_arr)}\r", end="")
 
-            plt.savefig(str(savepath), dpi=300)
+            plt.savefig(str(savepath), dpi=300, bbox_inches="tight")
             plt.close()
         
--- cy/spinup.py
+++ cy/spinup.py
@@ -18,38 +18,39 @@ size = MPI.COMM_WORLD.size
 ######### PARAMETERS ###############################################################
 
 # Simulation name
-sim_name = 'sim14'
+sim_name = 'sim17'
 
 # Numerical Parameters
-ns, nz = (512,1024)
+ns, nz = (1024,1024)
 dealias = 3/2
 dtype = np.float64
-timestepper = d3.RK443 #d3.RK222
+timestepper = d3.RK222 #d3.RK443 #d3.RK222
 
 # Physical parameters
 Ek = 5e-4 # Ekman number, Ek = nu/(Omega*H**2)
-PeakOmega = -1 # Maximum (absolute) change in rotation rate
+PeakOmega = -0.75 # Maximum (absolute) change in rotation rate
 Lz = 1 # height of cylinder
-Ls = 1.5 # radius of cylinder
+Ls = 3 # radius of cylinder
 w = 0.05 #0.1 # thickness of top and bottom "lids"
 eta = 3e-4  # Volume penalty damping timescale (enforces no-slip at top and bottom), 
             # set eta << 1 or eta < Ek to be safe
 perturbation_amp = 1e-4 # RMSE amplitude of us, uz noise
+free_surface = True # whether to impose a stress-free, no-penetration condition at z=0
 
 # Boundary forcing function, i.e., how the tank rotation rate should vary with time, t
 # Examples:
 
 ## Spin-down:
-full_DelOmega_func = lambda t,PeakOmega : PeakOmega * (0.5*(1 + np.tanh((2*(-1e-2 + t))/5e-3)))
+# full_DelOmega_func = lambda t,PeakOmega : PeakOmega * (0.5*(1 + np.tanh((2*(-1e-2 + t))/5e-3)))
 
 ## Spin-down then spin-up:
-# full_DelOmega_func = lambda t,PeakOmega : PeakOmega * -1/np.cosh((t - 0.02*10)/(0.005*10))
+full_DelOmega_func = lambda t,PeakOmega : PeakOmega * (0.5*(np.tanh((2*(-1e-2 + t))/5e-3) + np.tanh(-((2*(t - 0.2))/(5e-3)))))
 
 ## Write your own function:
 # full_DelOmega_func = lambda t,PeakOmega : <your function of t>
 
 # Cadences and stop time
-timestep = 5e-6
+timestep = 1e-5
 output_cadence = 10
 stop_sim_time = 0.4
 snapshot_dt = stop_sim_time/1000
@@ -78,13 +79,27 @@ t = dist.Field()
 DelOmega_func = lambda t: full_DelOmega_func(t,PeakOmega)
 DelOmega = DelOmega_func(t)
 
+# Volume penalty terms
+mask = lambda x: 0.5*(1+np.tanh(-2*x))
+eps = (eta*Ek)**(1/2)
+delta = 2.64822828*eps # sharpness of mask for obstacle
+Kwall = dist.Field(name='Kwall', bases=(zbasis))
+Kwall['g'] = mask(-(zgrid - (Lz/2))/delta) + mask((zgrid - (-Lz/2))/delta)
+
 # Substitutions
 ds = lambda A: d3.Differentiate(A, coords['s'])
 dz = lambda A: d3.Differentiate(A, coords['z'])
-tank_integ = lambda A: 2*np.pi*d3.Integrate(s*A, ('z','s'))
-Vol = np.pi*Ls**2*(Lz+2*w)
+if free_surface:
+    Klowerhalf = dist.Field(name='Klowerhalf', bases=(zbasis))
+    Klowerhalf['g'] = mask((zgrid)/delta)
+    tank_integ = lambda A: 2*np.pi*d3.Integrate(Klowerhalf*s*A, ('z','s'))
+    Vol = np.pi*Ls**2*(Lz/2+w)
+    integ_z = lambda A: d3.Integrate(Klowerhalf*A, ('z'))
+else:
+    tank_integ = lambda A: 2*np.pi*d3.Integrate(s*A, ('z','s'))
+    Vol = np.pi*Ls**2*(Lz+2*w)
+    integ_z = lambda A: d3.Integrate(A, ('z'))
 tank_avg = lambda A: 1/Vol * tank_integ(A)
-integ_z = lambda A: d3.Integrate(A, ('z'))
 
 # Tau terms p, us2, uphi2, uz2
 tau_p = dist.Field(name='tau_p', bases=zbasis)
@@ -97,20 +112,11 @@ ds_us = ds(us)
 ds_uphi = ds(uphi)
 ds_uz = ds(uz)
 
-# Volume penalty terms
-mask = lambda x: 0.5*(1+np.tanh(-2*x))
-eps = (eta*Ek)**(1/2)
-delta = 2.64822828*eps # sharpness of mask for obstacle
-Kceil = dist.Field(name='Kceil', bases=(zbasis))
-Kfloor = dist.Field(name='Kfloor', bases=(zbasis))
-Kceil['g'] = mask(-(zgrid - (Lz/2))/delta)
-Kfloor['g'] = mask((zgrid - (-Lz/2))/delta)
-
 # Initial condition: incompressible noise in us, uz
 psi = dist.Field(name='psi', bases=(zbasis,sbasis))
 psi.fill_random('g', seed=42, distribution='normal')
 psi.low_pass_filter(scales=0.25)
-psi['g'] = (psi * s/Ls * np.tanh(-(s - Ls)/0.1) * (1-(Kceil+Kfloor)))['g']
+psi['g'] = (psi * s/Ls * np.tanh(-(s - Ls)/0.1) * (1-(Kwall)))['g']
 us.change_scales(dealias)
 uz.change_scales(dealias)
 us['g'] = (dz(psi))['g']
@@ -119,12 +125,25 @@ ubot_rmse = np.sqrt(tank_avg(us**2 + uz**2))
 us['g'] = (us * 1/ubot_rmse * perturbation_amp)['g']
 uz['g'] = (uz * 1/ubot_rmse * perturbation_amp)['g']
 
+# Enforce stress-free, no-penetration condition spectrally
+if free_surface:
+    us['c'][1::2] = 0 # Set sines to 0 --> cosines only
+    uphi['c'][1::2] = 0 # Set sines to 0 --> cosines only
+    p['c'][1::2] = 0 # Set sines to 0 --> cosines only
+    tau_us2['c'][1::2] = 0 # Set sines to 0 --> cosines only
+    tau_uphi2['c'][1::2] = 0 # Set sines to 0 --> cosines only
+    tau_p['c'][1::2] = 0 # Set sines to 0 --> cosines only
+    Kwall['c'][1::2] = 0 # Set sines to 0 --> cosines only
+
+    uz['c'][::2] = 0 # Set cosines to 0 --> sines only
+    tau_uz2['c'][::2] = 0 # Set cosines to 0 --> sines only
+
 # Problem
 problem = d3.IVP([p, us, uphi, uz, tau_p, tau_us2, tau_uphi2, tau_uz2], time=t, namespace=locals())
 ## Equations
-problem.add_equation("-2*s**2*uphi + Ek*us + s**2*ds(p) - Ek*s*ds_us - Ek*s**2*ds(ds_us) + np.sqrt(Ek)*s**2*dt(us) - Ek*s**2*dz(dz(us)) + s**2*lift(tau_us2) = s*uphi**2 - s**2*us*ds_us - s**2*uz*dz(us) - s**2*((Kceil+Kfloor)*us)/eta")
-problem.add_equation("Ek*uphi + 2*s**2*us - Ek*s*ds_uphi - Ek*s**2*ds(ds_uphi) + np.sqrt(Ek)*s**2*dt(uphi) - Ek*s**2*dz(dz(uphi)) + s**2*lift(tau_uphi2) = -(s*uphi*us) - s**2*us*ds_uphi - s**2*uz*dz(uphi) - s**2*((Kceil+Kfloor)*(uphi-DelOmega*s))/eta")
-problem.add_equation("-(Ek*ds_uz) - Ek*s*ds(ds_uz) + np.sqrt(Ek)*s*dt(uz) + s*dz(p) - Ek*s*dz(dz(uz)) + s*lift(tau_uz2) = -(s*us*ds_uz) - s*uz*dz(uz) - s*((Kceil+Kfloor)*uz)/eta")
+problem.add_equation("-2*s**2*uphi + Ek*us + s**2*ds(p) - Ek*s*ds_us - Ek*s**2*ds(ds_us) + np.sqrt(Ek)*s**2*dt(us) - Ek*s**2*dz(dz(us)) + s**2*lift(tau_us2) = s*uphi**2 - s**2*us*ds_us - s**2*uz*dz(us) - s**2*((Kwall)*us)/eta")
+problem.add_equation("Ek*uphi + 2*s**2*us - Ek*s*ds_uphi - Ek*s**2*ds(ds_uphi) + np.sqrt(Ek)*s**2*dt(uphi) - Ek*s**2*dz(dz(uphi)) + s**2*lift(tau_uphi2) = -(s*uphi*us) - s**2*us*ds_uphi - s**2*uz*dz(uphi) - s**2*((Kwall)*(uphi-DelOmega*s))/eta")
+problem.add_equation("-(Ek*ds_uz) - Ek*s*ds(ds_uz) + np.sqrt(Ek)*s*dt(uz) + s*dz(p) - Ek*s*dz(dz(uz)) + s*lift(tau_uz2) = -(s*us*ds_uz) - s*uz*dz(uz) - s*((Kwall)*uz)/eta")
 problem.add_equation("us + s*ds_us + s*dz(uz) + tau_p = 0")
 ## BCs
 problem.add_equation("us(s=Ls) = 0")
@@ -164,7 +183,7 @@ if rank == 0:
     # Save parameters
     params_dict = {'ns':ns,'nz':nz,'timestep':timestep,
                    'Ek':Ek,'PeakOmega':PeakOmega,'Lz':Lz,'Ls':Ls,
-                   'w':w,'eta':eta,'perturbation_amp':perturbation_amp}
+                   'w':w,'eta':eta,'perturbation_amp':perturbation_amp,'free_surface':free_surface}
     params_json = json.dumps(params_dict, indent = 4)
     with open(save_params_path, "w") as outfile: 
         outfile.write(params_json)
@@ -180,10 +199,11 @@ snapshots.add_task(dz(us)-ds(uz),name='vort_phi')
 snapshots.add_task(tank_avg(uphi),name='avg_uphi')
 snapshots.add_task(0.5*tank_avg(uphi**2),name='E_phi')
 snapshots.add_task(0.5*tank_avg(us**2 + uz**2),name='E_sz')
+snapshots.add_task(ds_us,name='ds_us')
+snapshots.add_task(dz(uz),name='dz_uz')
 
 static_fields = solver.evaluator.add_file_handler(str(static_fields_path), sim_dt = stop_sim_time)
-static_fields.add_task(Kceil)
-static_fields.add_task(Kfloor)
+static_fields.add_task(Kwall)
 
 # Flow properties
 flow = d3.GlobalFlowProperty(solver, cadence=output_cadence)
@@ -196,6 +216,17 @@ try:
     logger.info('Starting loop')
     while solver.proceed:
         solver.step(timestep)
+        if free_surface:
+            us['c'][1::2] = 0 # Set sines to 0 --> cosines only
+            uphi['c'][1::2] = 0 # Set sines to 0 --> cosines only
+            p['c'][1::2] = 0 # Set sines to 0 --> cosines only
+            tau_us2['c'][1::2] = 0 # Set sines to 0 --> cosines only
+            tau_uphi2['c'][1::2] = 0 # Set sines to 0 --> cosines only
+            tau_p['c'][1::2] = 0 # Set sines to 0 --> cosines only
+
+            uz['c'][::2] = 0 # Set cosines to 0 --> sines only
+            tau_uz2['c'][::2] = 0 # Set cosines to 0 --> sines only
+
         if (solver.iteration-1) % output_cadence == 0:
             logger.info('Iteration: %i, Time: %e, dt: %e' %(solver.iteration, solver.sim_time, timestep))
             logger.info('Max |uphi| = {}, Max |uz| = {}, Max |p| = {}'.format(flow.max('abs_uphi'), flow.max('abs_uz'), flow.max('abs_p')))
